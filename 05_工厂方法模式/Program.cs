36585ea76f442b6e99d917c4335f413c42cf291f
zebin <237041086@qq.com>
2019-07-09 08:17:33 +0800

    7_9practive

---
+++ 05_工厂方法模式/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_工厂方法模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodFactory fishFood=new FishFoodFactory(new TunnyFish(18));
+            Fish tunnyFish=    fishFood.CreateFood() as Fish;
+         
+            FoodFactory fruitFoodFactory= new FruitFoodFactoru(new Jackfruit(200));
+            fruitFoodFactory.CreateFood();
+            
+           ILeifengFactory factory=new UndergraduateFactory();
+
+          Leifeng student=  factory.CreateLeifeng();
+           
+          student.BuyRice();
+          student.Wash();
+          student.Sweep();
+          factory=new VoluteerFactory();
+          Leifeng voluteer= factory.CreateLeifeng();
+            voluteer.Sweep();
+            voluteer.BuyRice();
+            voluteer.Wash();
+        }
+    }
+
+    interface FoodFactory
+    {
+        IFood CreateFood();
+    }
+
+    public class Leifeng
+    {
+        public void Wash()
+        {
+            Console.WriteLine("洗衣服");
+        }
+
+        public void Sweep()
+        {
+            Console.WriteLine("扫地");
+        }
+
+        public void BuyRice()
+        {
+            Console.WriteLine("买米");
+        }
+
+    }
+    //社区志愿者
+    class Voluteer:Leifeng
+    {
+        
+    }
+
+
+    public class StudentA : Leifeng
+    {
+    }
+
+    class UndergraduateFactory:ILeifengFactory
+    {
+        /// <inheritdoc />
+        public Leifeng CreateLeifeng()
+        {
+          return  new StudentA();//学雷锋的学生
+        }
+    }
+
+    class VoluteerFactory : ILeifengFactory
+    {
+        /// <inheritdoc />
+        public Leifeng CreateLeifeng()
+        {
+            return  new  Voluteer();
+        }
+    }
+
+    public  interface IFood
+    {
+        
+    }
+    public interface ILeifengFactory
+    {
+        Leifeng CreateLeifeng();
+    }
+ 
+
+    public abstract class Fish:IFood
+    {
+
+        public enum FishType
+        {
+            Amphiprion,//双锯鱼 属
+            Osteichthyes,//硬骨鱼纲
+            Trichiurus//带鱼属
+        }
+        protected int price;
+
+        public int Price
+        {
+            get => price;
+            set => price = value;
+        }
+
+        protected string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Fish()
+        {
+        }
+    }
+
+    
+   public class HairtailFish : Fish
+   {
+       /// <inheritdoc />
+       public HairtailFish(int price)
+       {
+           this.name = "HairtailFish";
+           this.price = price;
+       }
+   }
+
+   public class TunnyFish : Fish
+   {
+       /// <inheritdoc />
+       public TunnyFish(int price)
+       {
+           this.name = "TunnyFish";
+           this.price = price;
+       }
+   }
+
+   public class FishFoodFactory : FoodFactory
+    {
+        private Fish fish;
+
+        /// <inheritdoc />
+        public FishFoodFactory(Fish fish)
+        {
+            this.fish = fish;
+        }
+
+        /// <inheritdoc />
+        public IFood CreateFood(  )
+        {
+            Console.WriteLine("生成了:" + fish.Name + "...价格:" + fish.Price);
+
+            return fish;
+        }
+    }
+
+   
+   
+   public abstract class Fruit : IFood
+    {
+       public string name;
+       public int price;
+   }
+
+   public class Jackfruit : Fruit
+   {
+       /// <inheritdoc />
+       public Jackfruit(int price)
+       {
+           this.name = "Jackfruit";
+           this.price = price;
+       }
+   }
+
+   public class MangoFruit: Fruit
+   {
+       /// <inheritdoc />
+       public MangoFruit(int price)
+       {
+           name = "Mango";
+           this.price = price;
+       }
+   }
+
+   public class FruitFoodFactoru : FoodFactory
+   {
+       private Fruit _fruit;
+
+       /// <inheritdoc />
+       public FruitFoodFactoru(Fruit fruit)
+       {
+           _fruit = fruit;
+       }
+
+       /// <inheritdoc />
+       public IFood CreateFood()
+       {
+         Console.WriteLine("生产了"+ _fruit.name+"花费了"+_fruit.price);
+         return _fruit;
+       }
+   }
+   
+
+}
---
+++ 06_原型模式/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_原型模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteProtoType1 c1=new ConcreteProtoType1(2);
+            c1.b = "1234";
+            ConcreteProtoType1 c2=c1.Clone() as ConcreteProtoType1;
+            c1.b = "12345";
+            Console.WriteLine(c1.Id+c1.b);
+            Console.WriteLine(c2.Id+c2.b);
+
+            StudentResume resumeA=new StudentResume("小灰","23","喜欢算法和架构");
+
+            StudentResume resumeAb = resumeA.Clone() as StudentResume;
+            ;
+
+            Console.WriteLine(resumeA);
+            Console.WriteLine(resumeAb);
+
+        }
+    }
+
+    abstract class ProtoType
+    {
+        private int id;
+
+        public int Id
+        {
+            get => id;
+        }
+
+        /// <inheritdoc />
+        protected ProtoType(int id)
+        {
+            this.id = id;
+        }
+
+       public abstract ProtoType Clone();
+
+    }
+
+    class ConcreteProtoType1:ProtoType
+    {
+        public string b = "";
+        /// <inheritdoc />
+        public ConcreteProtoType1(int id) : base(id)
+        {
+        }
+
+        /// <inheritdoc />
+        public override ProtoType Clone()
+        {
+            return (ProtoType) this.MemberwiseClone();
+        }
+    }
+
+    public abstract class Resume
+    {
+        private string name;
+        private string id;
+        private string intro;
+
+        public string Name
+        {
+            get => name;
+             
+        }
+
+        public string Id
+        {
+            get => id;
+             
+        }
+
+        public string Intro
+        {
+            get => intro;
+             
+        }
+
+        /// <inheritdoc />
+        protected Resume(string name, string id, string intro)
+        {
+            this.name = name;
+            this.id = id;
+            this.intro = intro;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return  this.Name + this.intro;
+        }
+
+        public abstract Resume Clone();
+    }
+
+    public class  StudentResume:Resume
+    {
+        /// <inheritdoc />
+        public StudentResume(string name, string id, string intro) : base(name, id, intro)
+        {
+        }
+
+        /// <inheritdoc />
+        public override Resume Clone()
+        {
+            return this.MemberwiseClone() as StudentResume;
+        }
+    }
+}
