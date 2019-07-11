db3a7fc5e6504740e2b4e0a9767157d757d06ad9
zebin <237041086@qq.com>
2019-07-11 08:27:07 +0800

    模板方法设计模式

--- 06_原型模式/Program.cs
+++ 06_原型模式/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 数据集 对象 DataSet 也使用了原型模式， 通过Clone() 和Copy方法，区分 浅复制和深复制
+/// Clone 方法用来复制 Dataset 的数据结构，但不复制它的数据，实现 原型模式的浅复制， Copy方法，不但复制结构也复制数据。
+/// </summary>
 namespace _06_原型模式
 {
     class Program
@@ -17,13 +21,14 @@ namespace _06_原型模式
             Console.WriteLine(c1.Id+c1.b);
             Console.WriteLine(c2.Id+c2.b);
 
-            StudentResume resumeA=new StudentResume("小灰","23","喜欢算法和架构");
+            /*StudentResume resumeA=new StudentResume("小灰","23","喜欢算法和架构");
 
             StudentResume resumeAb = resumeA.Clone() as StudentResume;
             ;
 
             Console.WriteLine(resumeA);
             Console.WriteLine(resumeAb);
+*/
 
         }
     }
@@ -62,11 +67,58 @@ namespace _06_原型模式
         }
     }
 
-    public abstract class Resume
+    interface IClonable
+    {
+        object Clone();
+     
+    }
+    //这个就类 需要继承  Iconable, 否则 ，在使用mebershipClone 方法时，会造成，修改引用类型时 会影响原对象， 
+    public class WorkExperience:IClonable  // 参考深度复制 和 浅度复制
+    {
+        private string workData;
+
+        public string WorkData
+        {
+            get => workData;
+            set => workData = value;
+        }
+
+        private string company;
+
+        public string Company
+        {
+            get => company;
+            set => company = value;
+        }
+
+        /// <inheritdoc />
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+     
+    }
+    public class Resume:IClonable
     {
         private string name;
         private string id;
         private string intro;
+
+        private WorkExperience workExp;
+
+        public void SetWorkExperience(string workData,string Company)
+        {
+            workExp.Company = Company;
+            workExp.WorkData = workData;
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"name:{name}+intro:{intro}");
+            Console.WriteLine($"company:{workExp.Company}_data:{workExp.WorkData}");
+        }
 
         public string Name
         {
@@ -86,6 +138,12 @@ namespace _06_原型模式
              
         }
 
+        private Resume(WorkExperience work)
+        {   //提供Clone方法调用 的私有 构造函数，以便Clone
+            this.workExp=work.Clone() as WorkExperience;
+            ;
+        }
+
         /// <inheritdoc />
         protected Resume(string name, string id, string intro)
         {
@@ -100,7 +158,14 @@ namespace _06_原型模式
             return  this.Name + this.intro;
         }
 
-        public abstract Resume Clone();
+        public object Clone()
+        {
+            Resume obj=new Resume(this.workExp);
+            obj.name = this.Name;
+            obj.id = this.id;
+            obj.intro = this.intro;
+            return workExp.Clone();
+        }
     }
 
     public class  StudentResume:Resume
@@ -111,9 +176,5 @@ namespace _06_原型模式
         }
 
         /// <inheritdoc />
-        public override Resume Clone()
-        {
-            return this.MemberwiseClone() as StudentResume;
-        }
     }
 }
---
+++ 06_模板方法/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 减少 重复代码， 当要完成在某一细节层次一致的一个过程或一系列步骤，但其个别不走在更详细的层次上的实习可能不同时，通常考虑用模板方法模式处理
+/// </summary>
+namespace _07_模板方法
+{
+    abstract class AbstractTemplateMethod
+    {
+        public abstract void PrimitiveOpertation1();
+        public abstract void PrimitiveOpertation2();
+
+        public void TemplateMethod()
+        {
+            PrimitiveOpertation1();
+            PrimitiveOpertation2();
+            //给出逻辑的骨架， 推迟到子类实现。
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //同样的 试卷，但是 答案不同，  步骤都是相同的，答题，   细节不同，答案不同.
+            // 于此相同的 有 ， 每个人的 一日三餐， 都需要 吃饭， 但是 吃的饭 不一样。  吃早饭，吃午饭。吃晚饭。
+            StudentA a=new StudentA();
+            a.StartTest();
+            StudentB b=new StudentB();
+            b.StartTest();
+        }
+      
+    }
+    public abstract class TestPaper
+    {
+
+        public abstract void AnswerA();
+        public abstract void AnswerB();
+        public abstract void AnswerC();
+
+
+        void TestA()
+        {
+            Console.WriteLine("A1:答案是:");
+            AnswerA();
+        }
+        void TestB()
+        {
+            Console.WriteLine("A1:答案是:");
+            AnswerB();
+        }
+        void TestC()
+        {
+            Console.WriteLine("A1:答案是:");
+            AnswerC();
+        }
+
+        public void StartTest()
+        {
+            this.TestA();
+            this.TestB();
+            this.TestC();
+        }
+    }
+
+    public class StudentA : TestPaper
+    {
+        /// <inheritdoc />
+        public override void AnswerA()
+        {
+            Console.WriteLine("A");
+        }
+
+        /// <inheritdoc />
+        public override void AnswerB()
+        {
+            Console.WriteLine("B");
+
+        }
+
+        /// <inheritdoc />
+        public override void AnswerC()
+        {
+            Console.WriteLine("C");
+
+        }
+    }
+    public class StudentB : TestPaper
+    {
+        /// <inheritdoc />
+        public override void AnswerA()
+        {
+            Console.WriteLine("A");
+        }
+
+        /// <inheritdoc />
+        public override void AnswerB()
+        {
+            Console.WriteLine("A");
+
+        }
+
+        /// <inheritdoc />
+        public override void AnswerC()
+        {
+            Console.WriteLine("A");
+
+        }
+    }
+}
