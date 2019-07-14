1a6b495b3973ce9b3a93c3ab98a91b5334af1a5e
zebin <237041086@qq.com>
2019-07-14 23:33:59 +0800

    7.14

---
+++ 09_建造者模式/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 主要 用于创建一些复杂的对象，这些对象内部构建间的建造顺序通常是稳定的，但内部的构建通常面临复杂的变化
+/// </summary>
+namespace _09_建造者模式
+{  //有点类似模板方法 ， 区别 在于 建造者模式  多了一个建造者,Director  主要引用一个builder，build方法就是 统一它们的流程
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IBuild fatBuild=new PersonThinBuilder(09) as  IBuild;
+
+            PersonDirector personDirector=new PersonDirector();
+            personDirector.SetBuilder(fatBuild);
+            personDirector.BuildPeople();
+
+            Console.WriteLine("_________________________________________");
+            personDirector.SetBuilder(new PersonFatBuilder(90));
+            personDirector.BuildPeople();
+
+
+        }
+    }
+    /// <summary>
+    /// 建造者模式是在当创建复杂对象的算法应该独立于该对象的组成部分以及它们的装配方式时适用的模式
+    /// </summary>
+    public abstract class  IBuild
+    {
+        /// <inheritdoc />
+        protected IBuild(int buildId)
+        {
+            BuildId = buildId;
+        }
+
+        public  abstract void BuildHand();
+      public abstract void BuildHead();
+      public abstract void BuildFoot();
+      public abstract void BuildBody();
+
+      private int BuildId;
+
+      public int BuildId1
+      {
+          get => BuildId;
+          set => BuildId = value;
+      }
+    }
+    //具体建造者 实现builder接口 构造和装配各个部件，Product 就是那些具体的小人，产品角色
+    public class PersonThinBuilder : IBuild
+    {
+        /// <inheritdoc />
+        public PersonThinBuilder(int buildId) : base(buildId)
+        {
+        }
+
+        /// <inheritdoc />
+        public override void BuildHand()
+        {
+           Console.WriteLine("Thin hand");
+        }
+
+        /// <inheritdoc />
+        public override void BuildHead()
+        {
+            Console.WriteLine("Thin Head");
+        }
+
+        /// <inheritdoc />
+        public override void BuildFoot()
+        {
+            Console.WriteLine("Thin Foot");
+        }
+
+        /// <inheritdoc />
+        public override void BuildBody()
+        {
+             Console.WriteLine("Thin Body");
+        }
+    }
+    // 比如 建塔， 建房子,建人物
+    public class PersonFatBuilder:IBuild
+    {
+        /// <inheritdoc />
+        public PersonFatBuilder(int buildId) : base(buildId)
+        {
+        }
+
+        /// <inheritdoc />
+        public override void BuildHand()
+        {
+            Console.WriteLine("Fat hand");
+        }
+
+        /// <inheritdoc />
+        public override void BuildHead()
+        {
+            Console.WriteLine("Fat head");
+          }
+
+        /// <inheritdoc />
+        public override void BuildFoot()
+        {
+            Console.WriteLine("Fat foot");
+        }
+
+        /// <inheritdoc />
+        public override void BuildBody()
+        {
+            Console.WriteLine("Fat body");
+        }
+    }
+     //指挥者 用与创建一些复杂的对象，这些对象内部构建间的建造顺序通常是稳定的，但对象内部的构建通常面临着复杂变化
+    public class PersonDirector
+    {
+        private IBuild builder;
+
+        public void SetBuilder(IBuild builder)
+        {
+            this.builder = builder;
+        }
+
+        public void BuildPeople()
+        {
+            builder.BuildHead();
+            builder.BuildBody();
+            builder.BuildHand();
+            builder.BuildFoot();
+        }
+    }
+
+}
---
+++ _10_观察值模式/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_观察值模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observatory observatory=new Observatory();
+            GDob oGDob=new GDob(observatory,"广东天气台");
+            SHob sHob=new SHob(observatory,"上海天气台");
+            observatory.AddObserver(oGDob);
+            observatory.AddObserver(sHob);
+            observatory.Notify("现在是下雨");
+            observatory.WeatherNotify("现在转晴啦");
+        }
+    }
+
+    public interface ISubject
+    {
+        void Notify();
+        void AddObserver(Observer o);
+        void RemoveObserver(Observer o);
+        string State { get; set; }
+        void AddListner(WeatherNoticeEvent we);
+    }
+ 
+    public delegate void WeatherNoticeEvent(string weather);
+
+    /// <summary>
+    /// 气象台
+    /// </summary>
+    public class Observatory : ISubject
+    {
+        private List<Observer> observers;
+
+        //第二种 实现方式 使用 会比较简单,不需要在外界进行注册  
+        private WeatherNoticeEvent WE;  //如果经常需要移除 考虑用字典， 否则就 直接用 这个WeatherEvent 
+        private Dictionary<string, WeatherNoticeEvent> dic;
+
+        /// <inheritdoc />
+        public Observatory()
+        {
+            observers=new List<Observer>();
+        }
+
+        /// <summary>
+        /// 这种是 直接加
+        /// </summary>
+        /// <param name="we"></param>
+        public void AddListner(WeatherNoticeEvent we)
+        {
+           
+            if (WE==null)
+            {
+                WE = we; //首次
+            }
+            else
+            {
+                WE += we;
+            }
+        }
+
+        /// <inheritdoc />
+        public void Notify(string context)
+        {
+            foreach (var o in observers)
+            {
+                o.Update(context);
+            }
+        }
+
+        public void WeatherNotify(string weather)
+        {
+            if (WE!=null)
+            {
+                WE(weather);
+            }
+        }
+
+        /// <inheritdoc />
+        public void Notify()
+        {
+            foreach (var o in observers)
+            {
+                o.Update( );
+            }
+        }
+
+        /// <inheritdoc />
+        public void AddObserver(Observer o)
+        {
+            if (o!=null)
+            {
+                observers.Add(o);
+            }
+        }
+
+        /// <inheritdoc />
+        public void RemoveObserver(Observer o)
+        {
+            if (o!=null)
+            {
+                observers.Remove(o);
+            }
+        }
+
+        /// <inheritdoc />
+        public string State { get; set; }
+    }
+
+    public abstract class Observer
+    {
+        protected string name;
+        protected ISubject subject;
+        /// <inheritdoc />
+        protected Observer(ISubject subject,string name)
+        {
+             
+            this.subject = subject;
+            this.name = name;
+
+            //无法在构造函数里 直接添加观察者 但是 可以 利用委托 做到  
+            //   this.subject.AddObserver(this);
+            //利用委托
+            WeatherNoticeEvent ShWNE = new WeatherNoticeEvent(Update);
+
+            subject.AddListner(ShWNE);
+        }
+
+        public virtual void Update(string context)
+        {
+            Console.WriteLine(name + "提醒:" + context);
+
+        }
+
+        public abstract void Update();
+    }
+
+    public  class GDob : Observer
+    {
+        /// <inheritdoc />
+        public GDob(ISubject subject, string name) : base(subject, name)
+        {
+          
+        }
+
+        /// <inheritdoc />
+        public override void Update()
+        {}
+    }
+
+    public class SHob : Observer
+    {
+        // <inheritdoc/>
+        public SHob(ISubject subject, string name) : base(subject, name)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public override void Update()
+        {            
+        }
+
+    }
+}
