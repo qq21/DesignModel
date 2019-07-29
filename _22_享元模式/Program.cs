a36d836a2f65e7ab32a8e3bbc4a5a26ecc07e84b
zebin <237041086@qq.com>
2019-07-29 21:29:40 +0800

    _7_29

---
+++ _22_享元模式/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_享元模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int extrinsicstate = 22;
+            
+            FlyWeightFactory flyWeightFactory=new FlyWeightFactory();
+
+            FlyWeight fx = flyWeightFactory.GetFlyWeight("x");
+            fx.Operation(extrinsicstate);
+            FlyWeight ff = flyWeightFactory.GetFlyWeight("f");
+            ff.Operation(extrinsicstate);
+            FlyWeight fy = flyWeightFactory.GetFlyWeight("y");
+            fy.Operation(extrinsicstate);
+
+            WebFlyWeight wfw=new WebFlyWeight();
+            wfw.Start();
+
+        }
+    }
+
+    public abstract class FlyWeight
+    {
+        public abstract void Operation(int extrainsicstate);
+    }
+
+    public class ConcreteFlyWeight:FlyWeight
+    {
+        /// <inheritdoc />
+        public override void Operation(int extrainsicstate)
+        {
+         Console.WriteLine("具体FlyWeight"+extrainsicstate);
+        }
+    }
+
+    public class Concrete2FlyWeight:FlyWeight
+    {
+        /// <inheritdoc />
+        public override void Operation(int extrainsicstate)
+        {
+            Console.WriteLine("不具体实现的FlyWeiight"+extrainsicstate);
+        }
+    }
+
+    
+    public class FlyWeightFactory
+    {
+        Hashtable flyweights=new Hashtable();
+         
+        /// <inheritdoc />
+         public FlyWeightFactory()
+         {
+             flyweights.Add("x",new ConcreteFlyWeight());
+             flyweights.Add("f",new Concrete2FlyWeight());
+             flyweights.Add("y",new ConcreteFlyWeight());
+         }
+
+        public FlyWeight GetFlyWeight(string key)
+        {
+            return flyweights[key] as FlyWeight;
+        }
+    }
+
+
+
+}
---
+++ _22_享元模式/webFlyWeight.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_享元模式
+{
+    class WebFlyWeight
+    {
+
+        public void Start()
+        {
+            WebSiteFactory wf = new WebSiteFactory();
+            WebSite w1 = wf.GetWebSite("w1");
+            w1.Use( new User("咪咪"));
+            WebSite w2 = wf.GetWebSite("w2");
+            w2.Use(new User("小白"));
+            WebSite w3 = wf.GetWebSite("w3");
+            w3.Use(new User("路飞")); 
+
+            WebSite w4 = wf.GetWebSite("w4");
+            w4.Use(new User("鸣人"));
+            WebSite w5 = wf.GetWebSite("w5");
+             w4.Use(new User("六臂神童"));
+            WebSite w6 = wf.GetWebSite("w6")
+                ;
+            w6.Use(new User("后裔"));
+
+            Console.WriteLine($"网站总数:{wf.Count}");
+        }
+
+}
+
+    public abstract class WebSite
+    {
+        public abstract void Use(User user);
+    }
+
+    public class User
+    {
+        private string name;
+
+        /// <inheritdoc />
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get => name;
+        }
+    }
+
+    public class ConcretaWebSize:WebSite
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+        }
+
+        /// <inheritdoc />
+        public ConcretaWebSize(string name)
+        {
+            this.name = name;
+        }
+
+ 
+
+        /// <inheritdoc />
+        public override void Use(User user)
+        {
+            Console.WriteLine("网站分类:" + name+$"用户名：{user.Name}"); 
+        }
+    }
+
+    public class WebSiteFactory
+    {
+        private Hashtable flyWeights;
+
+        /// <inheritdoc />
+        public WebSiteFactory()
+        {
+            flyWeights=new Hashtable();
+        }
+
+        public WebSite GetWebSite(string key)
+        {
+            if (!flyWeights.ContainsKey(key))
+            {
+                flyWeights.Add(key, new ConcretaWebSize(key));
+            }
+            
+                return flyWeights[key] as WebSite;
+            
+        }
+
+        public int Count => flyWeights.Count;
+    }
+}
